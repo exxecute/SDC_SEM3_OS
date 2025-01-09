9456b1b94b156976be19d5ef2210ce7ef03925bb
execute <3xecvte@gmail.com>
2025-01-09 10:30:19 +0200

    done

--- lab2/main.c
+++ lab2/main.c
@@ -3,17 +3,11 @@
 
 #include "src/calculator/calculator.h"
 #include "src/io_array/io_array.h"
-
-int calculateSum(int* array, int size);
-float calculateAverage(int sum, int size);
-void findMinMax(int* array, int size, int* min, int* max);
-void countEvenOdd(int* array, int size, int* evenCount, int* oddCount);
-void bubbleSort(int* array, int size);
+#include "src/sorter/sorter.h"
 
 int main() {
     int n;
 
-    
     printf("Enter the number of elements: ");
     scanf("%d", &n);
 
@@ -22,38 +16,23 @@ int main() {
         return 1;
     }
 
-   
     int* array = (int*)malloc(n * sizeof(int));
     if (array == NULL) {
         printf("Memory allocation failed. Exiting program.\n");
         return 1;
     }
 
-    
     input_array(array, n);
 
     calculator_data_t calculator_data;
     calculator_count_all(array, n, &calculator_data);
     calculator_print(calculator_data);
    
-    bubbleSort(array, n);
+    sorter_bubble_sort(array, n);
     printf("Sorted array: ");
     print_array(array, n);
 
     free(array);
 
     return 0;
-}
-
-void bubbleSort(int* array, int size) {
-    for (int i = 0; i < size - 1; i++) {
-        for (int j = 0; j < size - i - 1; j++) {
-            if (array[j] > array[j + 1]) {
-                // Swap array[j] and array[j + 1]
-                int temp = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = temp;
-            }
-        }
-    }
 }
---
+++ lab2/src/sorter/sorter.h
@@ -0,0 +1,6 @@
+#ifndef SORTER_H
+#define SORTER_H
+
+void sorter_bubble_sort(int* array, int size);
+
+#endif // SORTER_H
